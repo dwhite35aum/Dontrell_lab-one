3f4efdbefda0e304f65b3cec2e033fe75018e08c
dwhite35aum <dwhite35@aum.edu>
2026-04-06 16:20:04 -0500

    vf2

--- Dontrell_lab one/Dontrell_lab one.cpp
+++ Dontrell_lab one/Dontrell_lab one.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello from Dontrell White! This is my first Git commit.";
+    cout << "Hello from Dontrell White! This is my first Git commit." << endl;
     cout << "Git makes collaboration easy!" << endl;
     cout << "Changes made by Marshon!" << endl;
 }
