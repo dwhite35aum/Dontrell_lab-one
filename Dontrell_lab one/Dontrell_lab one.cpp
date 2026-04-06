7f008fa228977e95c133de45e1dde97d73ae3e2d
dwhite35aum <dwhite35@aum.edu>
2026-04-06 16:32:49 -0500

    Final mini-task update

--- Dontrell_lab one/Dontrell_lab one.cpp
+++ Dontrell_lab one/Dontrell_lab one.cpp
@@ -9,4 +9,5 @@ int main()
     cout << "Hello from Dontrell White! This is my first Git commit." << endl;
     cout << "Git makes collaboration easy!" << endl;
     cout << "Changes made by Marshon!" << endl;
+    cout << "Lab 1 completed on 4-6-26!" << endl;
 }
