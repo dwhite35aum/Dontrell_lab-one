d93f11c82377925dc54958cf4825b28ca2678f86
dwhite35aum <dwhite35@aum.edu>
2026-04-08 15:25:24 -0500

    .....

--- Dontrell_lab one/Dontrell_lab one.cpp
+++ Dontrell_lab one/Dontrell_lab one.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main()
 {
     cout << "Hello from Marshon!" << endl;
+    cout << "Hello from Dontrell White!" << endl;
     cout << "Git makes collaboration easy!" << endl;
     cout << "Changes made by Marshon!" << endl;
     cout << "Lab 1 completed on 4-6-26!" << endl;
