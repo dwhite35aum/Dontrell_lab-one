fcafe907ac99efbacfed31aab00d76409ca33312
dwhite35aum <dwhite35@aum.edu>
2026-04-08 16:04:37 -0500

    Revert "Accidental bad commit"
    This reverts commit 1ff4d54567bdf1be8960e40cc56a8ff493f39be7.

--- Dontrell_lab one/Dontrell_lab one.cpp
+++ Dontrell_lab one/Dontrell_lab one.cpp
@@ -13,5 +13,4 @@ int main()
     cout << "Lab 1 completed on 4-6-26!" << endl;
     cout << "Feature by Dontrell White: Branching is awesome!" << endl;
     cout << "Student ID: 12345 | Name: Dontrell White" << endl;
-    cout << "TYPO LINE!!" << endl
 }
